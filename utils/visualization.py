2095a7358c13373b0255503f520373665a27753a
dhirendrasingh300 <dhirendrakumarsingh6306@gmail.com>
2026-06-30 14:50:35 +0530

    Add files via upload

---
+++ utils/visualization.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+import logging
+
+logger = logging.getLogger(__name__)
+
+def percentile_stretch(image, low=2, high=98):
+    """
+    Stretches the intensity of an image based on percentiles to remove outliers.
+    """
+    if image.ndim == 3:
+        stretched = np.zeros_like(image)
+        for i in range(image.shape[-1]):
+            stretched[..., i] = percentile_stretch(image[..., i], low, high)
+        return stretched.astype(np.uint8)
+    
+    image= np.array(image)
+
+    
+    low_val = np.percentile(image, low)
+    ###================ modified for removing noice from corners
+
+    # while low_val==0:
+    #     low=low +2
+    #     low_val = np.percentile(image, low)
+
+
+    
+    high_val = np.percentile(image, high)
+
+    
+    stretched = np.clip(image, low_val, high_val)
+    stretched = (stretched - low_val) * 255.0 / (high_val - low_val + 1e-5)
+    
+    return stretched.astype(np.uint8)
